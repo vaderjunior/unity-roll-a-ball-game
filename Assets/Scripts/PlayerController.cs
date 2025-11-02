5d7d652f608729c7f1887b6c28d749470d44b338
Ajay T Jose <ajaythoomkuzhy@gmail.com>
2025-11-02 18:06:36 +0100

    Added enemy and counter and win

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public float timeTaken = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
 
+    private int count;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {   
+        timeTaken = 0;
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+
     }
 
     void OnMove(InputValue movementValue)
@@ -23,6 +35,17 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 9)
+        {   
+            timeTaken = Time.timeSinceLevelLoad;
+            winTextObject.GetComponent<TMP_Text>().text = $"You Win!\nTime: {timeTaken:0.00}s";
+            winTextObject.SetActive(true);
+            
+        }
+    }
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
@@ -34,6 +57,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
     }
 }
